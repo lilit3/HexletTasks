f6e883f194dc38616d7c4d129e55765daeff190c
lilit3 <43044509+lilit3@users.noreply.github.com>
2018-12-03 15:25:08 +0200

    Update digits.php

--- digits.php
+++ digits.php
@@ -5,12 +5,10 @@ function addDigits($num)
 {
     $numbers = (string)$num;
     $res = 0;
-
-        $len = strlen($numbers);
+    $len = strlen($numbers);
         
         for ($i = 0; $i < $len; $i++) {
             $res += $numbers[$i];
-    
         }
         
         if ($res > 10) {
